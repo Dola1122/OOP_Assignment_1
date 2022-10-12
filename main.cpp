db51ca779051773a388ec54c902d113370a02990
Ahmed Adel <dolagamer2002@gmail.com>
2022-10-12 07:06:21 +0200

    program test structure

--- main.cpp
+++ main.cpp
@@ -3,31 +3,64 @@
 
 using namespace std;
 
-struct BigDecimalInt {
+
+// اكتبو اسماء الفانكشن و الفاريبلز بال camelCase , myVariableNameIsLikeThis
+// اسماء الclasses بال capitalized , MyClassName
+
+class BigDecimalInt {
+public:
     string num;
+    int signOfNumber = 1; // I think it's value should be 1 or -1 only
+    int sizeOfNumber;
+
+    BigDecimalInt(string decStr) {
+        this->num = decStr;
+    };
+
+    //initialize from integer constructor : I don't know how to do it btw
+    BigDecimalInt (int decInt);
+
+
+    // دي ميثود الجمع انا كنت حاولت فيها قبل كدا بس كملو انتو بقا
+    BigDecimalInt operator+(BigDecimalInt anotherDec) {
+        BigDecimalInt sum("");
+        int carry = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int sum_of_digit;
+            sum_of_digit = int(num[i] - '0') + int(anotherDec.num[i] - '0') + carry;
+            sum.num = to_string((sum_of_digit % 10)) + sum.num;
+            carry = sum_of_digit / 10;
+        }
+        if (carry > 0) {
+            sum.num = to_string(carry) + sum.num;
+        }
+        return sum;
+    }
+
+
+    BigDecimalInt operator-(BigDecimalInt anotherDec);
+
+    bool operator<(BigDecimalInt anotherDec);
+
+    bool operator>(BigDecimalInt anotherDec);
+
+    bool operator==(BigDecimalInt anotherDec);
+
+    BigDecimalInt operator=(BigDecimalInt anotherDec);
+
+    int size();
+
+    int sign();
+
+    // You will also need to overwrite the << operator as follows: (as friend or external function)
+    // k.
+    friend ostream &operator<<(ostream &out, BigDecimalInt b);
+
 };
 
-BigDecimalInt operator+(BigDecimalInt num1, BigDecimalInt num2) {
-    BigDecimalInt sum;
-    sum.num = "";
-    int carry = 0;
-    for (int i = num1.num.length() - 1; i >= 0; i--) {
-        int sum_of_digit;
-        sum_of_digit = int(num1.num[i] - '0') + int(num2.num[i] - '0') + carry;
-        sum.num = to_string((sum_of_digit % 10)) + sum.num;
-        carry = sum_of_digit / 10;
-    }
-    if (carry > 0) {
-        sum.num = to_string(carry) + sum.num;
-    }
-    return sum;
-}
-
 int main() {
-    BigDecimalInt num1;
-    num1.num = "99910356";
-    BigDecimalInt num2;
-    num2.num = "12899837";
+    BigDecimalInt num1("99910356");
+    BigDecimalInt num2("12899837");
     BigDecimalInt num3 = num1 + num2;
     cout << num3.num << endl;
     return 0;
